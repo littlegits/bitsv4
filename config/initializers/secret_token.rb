da9f255325314d7c4ffa9b77f7b834b35a309b9f
Bent Cardan <bent@kickr.io>
2012-08-31 04:27:22 -0400

    early morning commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitsv4::Application.config.secret_token = 'dc436719d0cd3a2555faca66a762d7f0def682b91da34fcee66ca391c48ca338115dd9ba9dc20855879ce3d085c1583aa7697c65fadabc7423979f0304eb5491'
