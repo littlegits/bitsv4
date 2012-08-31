6ec119fd327320e5bfefadc18430b34f7531474b
Bent Cardan <bent@kickr.io>
2012-08-31 04:57:24 -0400

    a littleController

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,31 +1,40 @@
 class WelcomeController < ApplicationController
-  def index
+def index
   end
-
+  
   def about
+  	@about = "selected"
   end
-
+  
   def community
+  	@community = "selected"
   end
-
+  
   def dreambits
+  	@dreambits = "selected"
   end
-
+  
   def forum
+  	@forum = "selected"
   end
-
-  def snapshots
-  end
-
+  
   def newsbits
+  	@newsbits = "selected"
   end
-
+  
+  def snapshots
+  	@snapshots = "selected"
+  end
+  
   def store
+  	@store = "selected"
   end
-
+  
   def thebits
+  	@thebits = "selected"
   end
-
+  
   def thekits
+  	@thekits = "selected"
   end
 end
